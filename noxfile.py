80fa461a4ec26093cb9655a40f4b9d90400d07e2
nobbi1991 <48419518+nobbi1991@users.noreply.github.com>
2023-03-14 23:19:14 +0100

    added version check to nox-file

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,7 @@ except ImportError:
 import nox
 
 PYTHON_VERSION = "3.10"
-nox.options.sessions = ["coverage", "pylint"]
+nox.options.sessions = ["version_check", "coverage", "pylint"]
 
 
 class Nox(nose_helper.nox_checks.common.NoxBase):
@@ -40,24 +40,3 @@ def pylint(session):
 def version_check(session):
 	"""Check if version was updated."""
 	Nox(session).version_check()
-
-
-@nox.session(python=PYTHON_VERSION)
-def combined_merge(session: nox.sessions.Session):
-	"""Run all tests for merge."""
-	nox_helper = nose_helper.nox_checks.common.NoxBase(session)
-	nose_helper.nox_checks.common.run_combined_sessions(session, [
-		("coverage", nox_helper.coverage),
-		("pylint", nox_helper.pylint)
-	])
-
-
-@nox.session(python=PYTHON_VERSION)
-def combined_release(session: nox.sessions.Session):
-	"""Run all tests for release."""
-	pkg_nox = nose_helper.nox_checks.common.NoxBase(session)
-	nose_helper.nox_checks.common.run_combined_sessions(session, [
-		("coverage", pkg_nox.coverage),
-		("pylint", pkg_nox.pylint),
-		("version_check", pkg_nox.version_check)
-	])
