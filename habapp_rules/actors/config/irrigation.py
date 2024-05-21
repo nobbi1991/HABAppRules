9f38819e6f47be79de7b56afa7e42cae69ba5147
nobbi1991 <48419518+nobbi1991@users.noreply.github.com>
2024-05-21 22:22:06 +0200

    added pydantic models for irrigation

---
+++ habapp_rules/actors/config/irrigation.py
@@ -0,0 +1,32 @@
+"""Config models for irrigation actors."""
+import typing
+
+import HABApp.openhab.items
+import pydantic
+
+import habapp_rules.core.exceptions
+import habapp_rules.core.pydantic_base
+
+
+class IrrigationItems(habapp_rules.core.pydantic_base.ItemBase):
+	"""Items for irrigation rules."""
+	valve: HABApp.openhab.items.SwitchItem = pydantic.Field(..., description="valve item which will be switched")
+	active: HABApp.openhab.items.SwitchItem = pydantic.Field(..., description="item to activate the rule")
+	hour: HABApp.openhab.items.NumberItem = pydantic.Field(..., description="start hour")
+	minute: HABApp.openhab.items.NumberItem = pydantic.Field(..., description="start minute")
+	duration: HABApp.openhab.items.NumberItem = pydantic.Field(..., description="duration in minutes")
+	repetitions: HABApp.openhab.items.NumberItem | None = pydantic.Field(None, description="number of repetitions")
+	brake: HABApp.openhab.items.NumberItem | None = pydantic.Field(None, description="time in minutes between repetitions")
+
+	@pydantic.model_validator(mode="after")
+	def validate_model(self) -> typing.Self:
+		if (self.repetitions is None) != (self.brake is None):
+			raise AssertionError("If repeats item is given, also the brake item must be given!")
+
+		return self
+
+
+class IrrigationConfig(habapp_rules.core.pydantic_base.ConfigBase):
+	"""Config for irrigation actors."""
+	items: IrrigationItems
+	parameter: None =  None
