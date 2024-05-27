a6b7b9c751998b7863ea776ef96f27d4de1c1881
nobbi1991 <48419518+nobbi1991@users.noreply.github.com>
2024-05-27 22:07:44 +0200

    pydantic for exponential filter

--- tests/common/filter.py
+++ tests/common/filter.py
@@ -4,6 +4,7 @@ import unittest.mock
 
 import HABApp
 
+import habapp_rules.common.config.filter
 import habapp_rules.common.filter
 import tests.helper.oh_item
 import tests.helper.test_case_base
@@ -21,10 +22,31 @@ class TestExponentialFilter(tests.helper.test_case_base.TestCaseBase):
 		tests.helper.oh_item.add_mock_item(HABApp.openhab.items.NumberItem, "Unittest_Filtered", None)
 		tests.helper.oh_item.add_mock_item(HABApp.openhab.items.NumberItem, "Unittest_Filtered_2", None)
 
+		config = habapp_rules.common.config.filter.ExponentialFilterConfig(
+			items=habapp_rules.common.config.filter.ExponentialFilterItems(
+				raw="Unittest_Raw",
+				filtered="Unittest_Filtered"
+			),
+			parameter=habapp_rules.common.config.filter.ExponentialFilterParameter(
+				tau=10
+			)
+		)
+
+		config_increase = habapp_rules.common.config.filter.ExponentialFilterConfig(
+			items=habapp_rules.common.config.filter.ExponentialFilterItems(
+				raw="Unittest_Raw",
+				filtered="Unittest_Filtered_2"
+			),
+			parameter=habapp_rules.common.config.filter.ExponentialFilterParameter(
+				tau=100,
+				instant_increase=True
+			)
+		)
+
 		self._rule_run_mock = unittest.mock.MagicMock()
 		with unittest.mock.patch("HABApp.rule.rule._HABAppSchedulerView", return_value=self._rule_run_mock):
-			self.filter = habapp_rules.common.filter.ExponentialFilter("Unittest_Raw", "Unittest_Filtered", 10)
-			self.filter_increase = habapp_rules.common.filter.ExponentialFilter("Unittest_Raw", "Unittest_Filtered_2", 100, True)
+			self.filter = habapp_rules.common.filter.ExponentialFilter(config)
+			self.filter_increase = habapp_rules.common.filter.ExponentialFilter(config_increase)
 
 	def test__init__(self):
 		"""Test __init__."""
@@ -33,26 +55,20 @@ class TestExponentialFilter(tests.helper.test_case_base.TestCaseBase):
 			unittest.mock.call(None, 20.0, self.filter_increase._cb_cyclic_calculate_and_update_output)
 		])
 
-		self.assertEqual("Unittest_Raw", self.filter._item_raw.name)
-		self.assertEqual("Unittest_Raw", self.filter_increase._item_raw.name)
+		self.assertEqual("Unittest_Raw", self.filter._config.items.raw.name)
+		self.assertEqual("Unittest_Raw", self.filter_increase._config.items.raw.name)
 
-		self.assertEqual("Unittest_Filtered", self.filter._item_filtered.name)
-		self.assertEqual("Unittest_Filtered_2", self.filter_increase._item_filtered.name)
+		self.assertEqual("Unittest_Filtered", self.filter._config.items.filtered.name)
+		self.assertEqual("Unittest_Filtered_2", self.filter_increase._config.items.filtered.name)
 
 		self.assertEqual(0.2, self.filter._alpha)
 		self.assertEqual(0.2, self.filter_increase._alpha)
 
-		self.assertFalse(self.filter._instant_increase)
-		self.assertFalse(self.filter._instant_decrease)
+		self.assertFalse(self.filter._config.parameter.instant_increase)
+		self.assertFalse(self.filter._config.parameter.instant_decrease)
 
-		self.assertTrue(self.filter_increase._instant_increase)
-		self.assertFalse(self.filter_increase._instant_decrease)
-
-		# instant_increase and instant_decrease is set
-		logger_mock = unittest.mock.MagicMock()
-		with unittest.mock.patch("habapp_rules.core.logger.InstanceLogger", return_value=logger_mock):
-			self.filter = habapp_rules.common.filter.ExponentialFilter("Unittest_Raw", "Unittest_Filtered", 10, True, True)
-		logger_mock.warning.assert_called_once_with("instant_increase and instant_decrease was set to True. This deactivates the filter completely!")
+		self.assertTrue(self.filter_increase._config.parameter.instant_increase)
+		self.assertFalse(self.filter_increase._config.parameter.instant_decrease)
 
 	def test_cb_cyclic_calculate_and_update_output(self):
 		"""Test _cb_cyclic_calculate_and_update_output."""
@@ -75,7 +91,7 @@ class TestExponentialFilter(tests.helper.test_case_base.TestCaseBase):
 
 			self.filter._cb_cyclic_calculate_and_update_output()
 
-			self.assertEqual(test_case.expected_result, round(self.filter._item_filtered.value, 2))
+			self.assertEqual(test_case.expected_result, round(self.filter._config.items.filtered.value, 2))
 
 			if test_case.new_value is not None and test_case.previous_value is not None:
 				self.assertEqual(test_case.expected_result, round(self.filter._previous_value, 2))
@@ -103,11 +119,12 @@ class TestExponentialFilter(tests.helper.test_case_base.TestCaseBase):
 		]
 
 		for test_case in test_cases:
-			tests.helper.oh_item.set_state("Unittest_Filtered_2", 100)  # set some random value
-			self.filter_increase._previous_value = test_case.previous_value
-			self.filter_increase._instant_increase = test_case.instant_increase
-			self.filter_increase._instant_decrease = test_case.instant_decrease
+			with self.subTest(test_case=test_case):
+				tests.helper.oh_item.set_state("Unittest_Filtered_2", 100)  # set some "random" value
+				self.filter_increase._previous_value = test_case.previous_value
+				self.filter_increase._config.parameter.instant_increase = test_case.instant_increase
+				self.filter_increase._config.parameter.instant_decrease = test_case.instant_decrease
 
-			tests.helper.oh_item.item_state_change_event("Unittest_Raw", test_case.new_value)
+				tests.helper.oh_item.item_state_change_event("Unittest_Raw", test_case.new_value)
 
-			tests.helper.oh_item.assert_value("Unittest_Filtered_2", test_case.expected_value)
+				tests.helper.oh_item.assert_value("Unittest_Filtered_2", test_case.expected_value)
