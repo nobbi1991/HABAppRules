01d6d95e40b2bd999c8792e13c011315d96a5725
nobbi1991 <48419518+nobbi1991@users.noreply.github.com>
2024-05-27 14:44:43 +0200

    pydantic for knx_mqtt bridge

--- habapp_rules/bridge/knx_mqtt.py
+++ habapp_rules/bridge/knx_mqtt.py
@@ -3,6 +3,7 @@ import logging
 
 import HABApp
 
+import habapp_rules.bridge.config.knx_mqtt
 import habapp_rules.core.exceptions
 import habapp_rules.core.logger
 
@@ -20,36 +21,23 @@ class KnxMqttDimmerBridge(HABApp.Rule):
 	info: OpenHAB does not support start/stop dimming. Thus, this implementation will set fixed values if INCREASE/DECREASE was received from KNX
 	"""
 
-	def __init__(self, mqtt_dimmer: str, knx_switch_ctr: str | None = None, knx_dimmer_ctr: str | None = None, increase_value: int = 60, decrease_value: int = 30) -> None:
+	def __init__(self, config: habapp_rules.bridge.config.knx_mqtt.KnxMqttConfig) -> None:
 		"""Create object of KNX to MQTT bridge
 
-		:param mqtt_dimmer: name of MQTT item
-		:param knx_switch_ctr: name of KNX switch-control item
-		:param knx_dimmer_ctr: name of KNX dimmer-control item
-		:param increase_value: value which is set when INCREASE was received.
-		:param decrease_value: value which is set when DECREASE was received.
+		:param config: Configuration of the KNX MQTT bridge
 		:raises habapp_rules.core.exceptions.HabAppRulesConfigurationException: If config is not valid
 		"""
-		if knx_switch_ctr is None and knx_dimmer_ctr is None:
-			raise habapp_rules.core.exceptions.HabAppRulesConfigurationException(f"At least one KNX item must be given! knx_switch_ctr = {knx_switch_ctr} | knx_dimmer_ctr = {knx_dimmer_ctr}")
-
-		knx_name = knx_switch_ctr if knx_switch_ctr else knx_dimmer_ctr
+		self._config = config
+		knx_name = self._config.items.knx_switch_ctr.name if self._config.items.knx_switch_ctr is not None else self._config.items.knx_dimmer_ctr.name
 
 		HABApp.Rule.__init__(self)
-		self._instance_logger = habapp_rules.core.logger.InstanceLogger(LOGGER, f"{knx_name}__{mqtt_dimmer}")
+		self._instance_logger = habapp_rules.core.logger.InstanceLogger(LOGGER, f"{knx_name}__{self._config.items.mqtt_dimmer.name}")
 
-		self.__increase_value = increase_value
-		self.__decrease_value = decrease_value
-
-		self._mqtt_item = HABApp.openhab.items.DimmerItem.get_item(mqtt_dimmer)
-		self._knx_dimmer_item = HABApp.openhab.items.DimmerItem.get_item(knx_dimmer_ctr) if knx_dimmer_ctr else None
-		self._knx_switch_item = HABApp.openhab.items.SwitchItem.get_item(knx_switch_ctr) if knx_switch_ctr else None
-
-		self._mqtt_item.listen_event(self._cb_mqtt_event, HABApp.openhab.events.ItemStateChangedEventFilter())
-		if self._knx_dimmer_item is not None:
-			self._knx_dimmer_item.listen_event(self._cb_knx_event, HABApp.openhab.events.ItemCommandEventFilter())
-		if self._knx_switch_item is not None:
-			self._knx_switch_item.listen_event(self._cb_knx_event, HABApp.openhab.events.ItemCommandEventFilter())
+		self._config.items.mqtt_dimmer.listen_event(self._cb_mqtt_event, HABApp.openhab.events.ItemStateChangedEventFilter())
+		if self._config.items.knx_dimmer_ctr is not None:
+			self._config.items.knx_dimmer_ctr.listen_event(self._cb_knx_event, HABApp.openhab.events.ItemCommandEventFilter())
+		if self._config.items.knx_switch_ctr is not None:
+			self._config.items.knx_switch_ctr.listen_event(self._cb_knx_event, HABApp.openhab.events.ItemCommandEventFilter())
 		self._instance_logger.debug("successful!")
 
 	def _cb_knx_event(self, event: HABApp.openhab.events.ItemCommandEvent) -> None:
@@ -58,15 +46,15 @@ class KnxMqttDimmerBridge(HABApp.Rule):
 		:param event: HABApp event
 		"""
 		if isinstance(event.value, (int, float)):
-			self._mqtt_item.oh_send_command(event.value)
+			self._config.items.mqtt_dimmer.oh_send_command(event.value)
 		elif event.value in {"ON", "OFF"}:
-			self._mqtt_item.oh_send_command(event.value)
+			self._config.items.mqtt_dimmer.oh_send_command(event.value)
 		elif event.value == "INCREASE":
-			target_value = self.__increase_value if self._mqtt_item.value < self.__increase_value else 100
-			self._mqtt_item.oh_send_command(target_value)
+			target_value = self._config.parameter.increase_value if self._config.items.mqtt_dimmer.value < self._config.parameter.increase_value else 100
+			self._config.items.mqtt_dimmer.oh_send_command(target_value)
 		elif event.value == "DECREASE":
-			target_value = self.__decrease_value if self._mqtt_item.value > self.__decrease_value else 0
-			self._mqtt_item.oh_send_command(target_value)
+			target_value = self._config.parameter.decrease_value if self._config.items.mqtt_dimmer.value > self._config.parameter.decrease_value else 0
+			self._config.items.mqtt_dimmer.oh_send_command(target_value)
 		else:
 			self._instance_logger.error(f"command '{event.value}' ist not supported!")
 
@@ -75,8 +63,8 @@ class KnxMqttDimmerBridge(HABApp.Rule):
 
 		:param event: HABApp event
 		"""
-		if self._knx_dimmer_item is not None:
-			self._knx_dimmer_item.oh_post_update(event.value)
+		if self._config.items.knx_dimmer_ctr is not None:
+			self._config.items.knx_dimmer_ctr.oh_post_update(event.value)
 
-		if self._knx_switch_item is not None:
-			self._knx_switch_item.oh_post_update("ON" if event.value > 0 else "OFF")
+		if self._config.items.knx_switch_ctr is not None:
+			self._config.items.knx_switch_ctr.oh_post_update("ON" if event.value > 0 else "OFF")
