18b568791d76844a3ce6d3b921ce7a3b7de7bfd7
nobbi1991 <48419518+nobbi1991@users.noreply.github.com>
2024-11-16 23:10:15 +0100

    started with hatch and pre-commit

--- habapp_rules/__init__.py
+++ habapp_rules/__init__.py
@@ -1,4 +1,6 @@
 """Define all rules for HABApp"""
+
 import pathlib
 
+__version__ = "7.0.0"
 BASE_PATH = pathlib.Path(__file__).parent.parent.resolve()
--- habapp_rules/__version__.py
+++
@@ -1,2 +0,0 @@
-"""Set version for the package."""
-__version__ = "7.0.0"
