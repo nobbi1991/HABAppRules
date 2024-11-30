96fcde3571c917c9c49d42c59472abf0bdc446bc
nobbi1991 <48419518+nobbi1991@users.noreply.github.com>
2024-11-30 16:46:28 +0100

    added rules to avoid sun protection if heating period is active

--- habapp_rules/actors/config/heating.py
+++ habapp_rules/actors/config/heating.py
@@ -1,5 +1,7 @@
 """Config models for heating rules."""
 
+import datetime
+
 import HABApp.openhab.items
 import pydantic
 
@@ -19,3 +21,24 @@ class KnxHeatingConfig(habapp_rules.core.pydantic_base.ConfigBase):
 
     items: KnxHeatingItems = pydantic.Field(..., description="items for heating rule")
     parameter: None = None
+
+
+class HeatingActiveItems(habapp_rules.core.pydantic_base.ItemBase):
+    """Items for active heating rule."""
+
+    control_values: list[HABApp.openhab.items.NumberItem] = pydantic.Field(..., description="list of control value items")
+    output: HABApp.openhab.items.SwitchItem = pydantic.Field(..., description="output item, which is ON when at least one control value is above the threshold")
+
+
+class HeatingActiveParameter(habapp_rules.core.pydantic_base.ParameterBase):
+    """Parameters for active heating rule."""
+
+    threshold: float = pydantic.Field(0, description="control value threshold")
+    extended_active_time: datetime.timedelta = pydantic.Field(datetime.timedelta(days=1), description="extended time to keep the output item ON, after last control value change below the threshold")
+
+
+class HeatingActiveConfig(habapp_rules.core.pydantic_base.ConfigBase):
+    """Config for active heating rule."""
+
+    items: HeatingActiveItems = pydantic.Field(..., description="items for active heating rule")
+    parameter: HeatingActiveParameter = pydantic.Field(HeatingActiveParameter(), description="parameters for active heating rule")
--- tests/actors/heating.py
+++ tests/actors/heating.py
@@ -1,8 +1,11 @@
 """Test heating rules."""
 
 import collections
+import datetime
+import unittest.mock
 
 import HABApp
+import HABApp.rule.scheduler.job_ctrl
 
 import habapp_rules.actors.config.heating
 import habapp_rules.actors.heating
@@ -78,3 +81,79 @@ class TestKnxHeating(tests.helper.test_case_base.TestCaseBase):
 
                 self.assertEqual(test_case.expected_new_offset, round(self._rule._config.items.temperature_offset.value, 1))
                 self.assertEqual(test_case.event_value, self._rule._temperature)
+
+
+class TestHeatingActive(tests.helper.test_case_base.TestCaseBase):
+    """Test HeatingActive."""
+
+    def setUp(self) -> None:
+        """Setup test case."""
+        tests.helper.test_case_base.TestCaseBase.setUp(self)
+        tests.helper.oh_item.add_mock_item(HABApp.openhab.items.NumberItem, "Unittest_ctr_value_1", None)
+        tests.helper.oh_item.add_mock_item(HABApp.openhab.items.NumberItem, "Unittest_ctr_value_2", None)
+        tests.helper.oh_item.add_mock_item(HABApp.openhab.items.SwitchItem, "Unittest_heating_active_1", None)
+        tests.helper.oh_item.add_mock_item(HABApp.openhab.items.SwitchItem, "Unittest_heating_active_2", None)
+
+        self._config_1 = habapp_rules.actors.config.heating.HeatingActiveConfig(items=habapp_rules.actors.config.heating.HeatingActiveItems(control_values=["Unittest_ctr_value_1", "Unittest_ctr_value_2"], output="Unittest_heating_active_1"))
+        self._config_2 = habapp_rules.actors.config.heating.HeatingActiveConfig(
+            items=habapp_rules.actors.config.heating.HeatingActiveItems(control_values=["Unittest_ctr_value_1", "Unittest_ctr_value_2"], output="Unittest_heating_active_2"),
+            parameter=habapp_rules.actors.config.heating.HeatingActiveParameter(extended_active_time=datetime.timedelta(seconds=10), threshold=20),
+        )
+
+        self._rule_1 = habapp_rules.actors.heating.HeatingActive(self._config_1)
+        self._rule_2 = habapp_rules.actors.heating.HeatingActive(self._config_2)
+
+    def test_init(self) -> None:
+        """Test __init__."""
+        self.assertTrue(isinstance(self._rule_1._extended_lock, HABApp.rule.scheduler.job_ctrl.CountdownJobControl))
+        self.assertIsNone(self._rule_1._extended_lock.next_run_datetime)
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "OFF")
+        tests.helper.oh_item.assert_value("Unittest_heating_active_2", "OFF")
+        self.assertIn("_cb_lock_end", self._rule_1._extended_lock._job.executor._func.name)
+
+    def test_init_when_output_is_on(self) -> None:
+        """Test __init__ when output is on."""
+        TestCase = collections.namedtuple("TestCase", "state, reset_called")
+
+        test_cases = [TestCase("ON", True), TestCase("OFF", False), TestCase(None, False)]
+
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                tests.helper.oh_item.set_state("Unittest_heating_active_1", test_case.state)
+                with unittest.mock.patch("HABApp.rule.scheduler.job_builder.HABAppJobBuilder.countdown") as run_countdown_mock:
+                    habapp_rules.actors.heating.HeatingActive(self._config_1)
+                if test_case.reset_called:
+                    run_countdown_mock.return_value.reset.assert_called_once()
+                else:
+                    run_countdown_mock.return_value.reset.assert_not_called()
+
+    def test_set_output(self) -> None:
+        """Test _set_output."""
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "OFF")
+
+        # ctr_value_1 changes to 0
+        tests.helper.oh_item.item_state_change_event("Unittest_ctr_value_1", 0)
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "OFF")
+        tests.helper.oh_item.assert_value("Unittest_heating_active_2", "OFF")
+
+        # ctr_value_1 changes to 10
+        tests.helper.oh_item.item_state_change_event("Unittest_ctr_value_1", 10)
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "ON")
+        tests.helper.oh_item.assert_value("Unittest_heating_active_2", "OFF")
+
+        # ctr_value_2 changes to 21
+        tests.helper.oh_item.item_state_change_event("Unittest_ctr_value_2", 21)
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "ON")
+        tests.helper.oh_item.assert_value("Unittest_heating_active_2", "ON")
+
+        # both change to 0
+        tests.helper.oh_item.item_state_change_event("Unittest_ctr_value_1", 0)
+        tests.helper.oh_item.item_state_change_event("Unittest_ctr_value_2", 0)
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "ON")
+        tests.helper.oh_item.assert_value("Unittest_heating_active_2", "ON")
+
+        # lock period ends
+        self._rule_1._cb_lock_end()
+        self._rule_2._cb_lock_end()
+        tests.helper.oh_item.assert_value("Unittest_heating_active_1", "OFF")
+        tests.helper.oh_item.assert_value("Unittest_heating_active_2", "OFF")
